4d6bd5716c4c8902ef920ffcdb45a42e05f0882a
Ilija Radosavljevic <ikaikica.ir@gmail.com>
2022-11-25 21:15:56 +0100

    refatored

---
+++ dz2/IO.cpp
@@ -0,0 +1,59 @@
+#include "BPlusTree.h"
+#include "Helpers.h"
+
+BPlusTree* BPlusTree::FromFile(size_t m, std::string fname)
+{
+	BPlusTree* b = new BPlusTree(m);
+	std::ifstream infile;
+	infile.open(fname);
+	std::string line = "";
+	while (std::getline(infile, line)) {
+		Data* d = new Data();
+		std::string tmp_s = "";
+		std::stringstream ss(line);
+		std::getline(ss, tmp_s, '|');
+		d->ca_id = std::stoll(tmp_s) % 100;
+		std::getline(ss, tmp_s, '|');
+		d->ca_b_id = std::stoll(tmp_s);
+		std::getline(ss, tmp_s, '|');
+		d->ca_c_id = std::stoll(tmp_s);
+		std::getline(ss, d->ca_name, '|');
+		std::getline(ss, tmp_s, '|');
+		d->ca_tax_st = std::stoll(tmp_s);
+		std::getline(ss, tmp_s, '|');
+		d->ca_bal = std::stod(tmp_s);
+		b->Insert(d->ca_id, d);
+	}
+	infile.close();
+	return b;
+}
+
+void BPlusTree::Print()
+{
+	std::queue<Node*> q;
+	q.push(this->root);
+	while (!q.empty()) {
+		int cnt = q.size();
+		while (cnt) {
+			Node* t = q.front();
+			q.pop();
+			for (size_t i = 0; i < t->sz; i++)
+			{
+				if (!t->isLeaf) q.push((t->p.subtree[i]));
+			}
+			if (!t->isLeaf) q.push(t->p.subtree[t->sz]);
+			t->printNodeKeys();
+			std::cout << "-";
+			cnt--;
+		}
+		std::cout << std::endl << std::endl;
+
+	}
+}
+void helpers::writeNodeToFile(Data* d, std::ofstream& out)
+{
+	std::stringstream ss;
+	ss << std::fixed << std::setprecision(2) << d->ca_id << "|" << d->ca_b_id << "|" << d->ca_c_id << "|" << d->ca_name << "|" << d->ca_tax_st << "|" << d->ca_bal << std::endl;
+	out << ss.rdbuf();
+	return;
+}
---
+++ dz2/Search.cpp
@@ -0,0 +1,132 @@
+#include "BPlusTree.h"
+#include "Helpers.h"
+
+bool BPlusTree::SearchSingle(size_t key, Node*& leaf, Node*& parent, size_t* took_steps, size_t* idx)
+{
+	*took_steps = 1;
+	if (this->root == nullptr) return false;
+	Node* curr = this->root;
+	while (!curr->isLeaf) {
+		size_t i;
+		for (i = 0; i < curr->sz; i++)
+		{
+			if (key <= curr->keys[i]) {
+				parent = curr;
+				(*took_steps)++;
+				curr = curr->p.subtree[i];
+				break;
+			}
+		}
+		if (i == curr->sz)//if key > all keys in node
+		{
+			(*took_steps)++;
+			parent = curr;
+			curr = curr->p.subtree[i];
+		}
+
+
+	}
+	leaf = curr;
+	//we are at leaf level now, search inside leaf node for key	
+	for (size_t i = 0; i < curr->sz; i++) if (key == curr->keys[i]) {
+		*idx = i;
+		return true;
+	}
+	*idx = -1;
+	return false;
+}
+
+bool BPlusTree::SearchSingle(size_t key, Node*& leaf, Node*& parent)
+{
+	if (this->root == nullptr) return false;
+	Node* curr = this->root;
+	while (!curr->isLeaf) {
+		size_t i;
+		for (i = 0; i < curr->sz; i++)
+		{
+			if (key <= curr->keys[i]) {
+				parent = curr;
+				curr = curr->p.subtree[i];
+				break;
+			}
+		}
+		if (i == curr->sz)//if key > all keys in node
+		{
+			parent = curr;
+			curr = curr->p.subtree[i];
+		}
+
+	}
+	leaf = curr;
+	//we are at leaf level now, search inside leaf node for key	
+	for (size_t i = 0; i < curr->sz; i++) if (key == curr->keys[i]) return true;
+	return false;
+}
+
+Node* BPlusTree::getParent(size_t key)
+{
+	Node* parent = nullptr;
+	Node* curr = this->root;
+	if (this->root == nullptr) return nullptr;
+	while (!curr->isLeaf) {
+		size_t i;
+		for (i = 0; i < curr->sz; i++)
+		{
+			if (key == curr->keys[i]) {
+				return parent;
+			}
+			if (key < curr->keys[i]) {
+				parent = curr;
+				curr = curr->p.subtree[i];
+				break;
+			}
+		}
+		if (i == curr->sz)//if key > all keys in node
+		{
+			parent = curr;
+			curr = curr->p.subtree[i];
+		}
+
+
+	}
+	return nullptr;
+
+
+}
+
+void BPlusTree::SearchMultiple(std::vector<size_t>& keys, size_t* tookSteps, std::string filename)
+{
+	std::ofstream out(filename, std::ios_base::out | std::ios_base::trunc);
+	*tookSteps = 0;
+	for (size_t i = 0; i < keys.size(); i++)
+	{
+		size_t took_steps_i, idx, key;
+		Node* leaf, * junk;
+		if (SearchSingle(keys[i], leaf, junk, &took_steps_i, &idx)) {
+			*tookSteps += took_steps_i;
+			helpers::writeNodeToFile(leaf->p.data[idx], out);
+		}
+	}
+	out.close();
+}
+void BPlusTree::SearchMultipleSuccessive(size_t startKey, size_t k, size_t* took_steps, std::string filename)
+{
+	Node* curr, * junk;
+	size_t idx;
+	std::ofstream out(filename, std::ios_base::out | std::ios_base::trunc);
+	SearchSingle(startKey, curr, junk, took_steps, &idx);
+	size_t i = 0;
+	while (i < k && curr != nullptr) {
+		while (idx < curr->sz && i < k) {
+
+
+			helpers::writeNodeToFile(curr->p.data[idx], out);
+			idx++;
+			i++;
+		}
+		idx = 0;
+		curr = curr->p.subtree[curr->m - 1];//next in linked list
+		(*took_steps)++;
+	}
+	return;
+}
