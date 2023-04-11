bd60cd43fda34c54b4fd1509afb8c894e3f9496b
Ilija Radosavljevic <45182003+ilija02@users.noreply.github.com>
2023-04-11 15:26:30 +0200

    translated UI to English

--- dz2/Helpers.cpp
+++ dz2/Helpers.cpp
@@ -5,22 +5,22 @@ int helpers::getSelectedMenuOption()
 
 	int selectedOption;
 	std::cout << "\n----------------------------------------------------------------------------------------------\n";
-	std::cout << "\t1) Stvaranje i unistavanje indeksa nad tabelom CUSTOMER_ACCOUNT\n\
-	2) Ispis indeksa na standardni izlaz\n\
-	3) Dodavanje novog zapisa u tabelu\n\
-	4) Brisanje zapisa iz tabele\n\
-	5) Pretrazivanje jednog podatka\n\
-	6) Pretrazivanje k podataka\n\
-	7) Pretrazivanje k sukcesivnih podataka\n\
-	8) Brisanje indeksa iz memorije\n\
-	9) Kreiranje praznog stabla\n\
-	0) Kraj rada\n";
+	std::cout << "\t1) Create/Destroy index over CUSTOMER_ACCOUNT table\n\
+	2) Print index\n\
+	3) Add new record\n\
+	4) Delete record\n\
+	5) Search record\n\
+	6) Search k records\n\
+	7) Search k successive records\n\
+	8) Delete index from memory\n\
+	9) Create empty B+ tree\n\
+	0) Exit\n";
 	std::cout << "----------------------------------------------------------------------------------------------\n";
-	std::cout << "\033[1;33mUnos: \033[0m";
+	std::cout << "\033[1;33mInput: \033[0m";
 	std::cin >> selectedOption;
 	while (selectedOption < 0 || selectedOption > ZAD1_NR_OPTIONS) {
-		std::cout << "Neispravan unos, pokusajte opet ili unesite 0 za kraj\n";
-		std::cout << "\033[1;31m Unos: \033[0m";
+		std::cout << "Invalid input, try again or enter 0 to exit\n";
+		std::cout << "\033[1;31m Input: \033[0m";
 		std::cin >> selectedOption;
 	}
 	//system("cls");
@@ -37,18 +37,18 @@ void helpers::mainLoop() {
 			size_t m;
 			std::string fname;
 			do {
-				std::cout << "\tRed stabla m: ";
+				std::cout << "\tB+ tree order: ";
 				std::cin >> m;
 			} while (m < 3 || m>10);
 			//std::cout << "\tIme fajla: "; TODO: omoguciti unos imena fajla
 			
 			while (true) {
 				std::ifstream in;
-				std::cout << "\tIme (putanja) fajla: ";
+				std::cout << "\tFilename (Filepath): ";
 				std::cin >> fname;
 				in.open(fname);
 				if (in) break;
-				std::cout << "\033[1;31m\tGreska, fajl ne postoji\033[0m" << std::endl;
+				std::cout << "\033[1;31m\tError, file doesn't exist\033[0m" << std::endl;
 			}
 			b = BPlusTree::FromFile(m, fname);
 			
@@ -56,7 +56,7 @@ void helpers::mainLoop() {
 		}
 		case 2: {
 			if (!nullCheckAndError(b)) continue;
-			std::cout << "Ispis stabla: " << std::endl;
+			std::cout << "Tree: " << std::endl;
 			b->Print();
 			break;
 		}
@@ -76,7 +76,7 @@ void helpers::mainLoop() {
 			std::cout << "\tca_bal: ";
 			std::cin >> d->ca_bal;
 			if (!b->Insert(d->ca_id, d)) {
-				std::cout << std::endl << "\033[1;31mGreska, kljuc vec postoji\033[0m" << std::endl;
+				std::cout << std::endl << "\033[1;31mError, key already exists\033[0m" << std::endl;
 				continue;
 			}
 			//b->Print();
@@ -85,10 +85,10 @@ void helpers::mainLoop() {
 		case 4: {
 			size_t key;
 			if (!nullCheckAndError(b)) continue;
-			std::cout << "\tKljuc koji zelite da obrisete: ";
+			std::cout << "\tPrimary key of the record you want to delete: ";
 			std::cin >> key;
 			if (!b->Delete(key)) {
-				std::cout << std::endl << "\033[1;31m\tGreska, kljuc ne postoji\033[0m" << std::endl;
+				std::cout << std::endl << "\033[1;31m\tError, nonexistent key\033[0m" << std::endl;
 				continue;
 			}
 			break;
@@ -97,14 +97,14 @@ void helpers::mainLoop() {
 			if (!nullCheckAndError(b)) continue;
 			size_t took_steps, idx, key;
 			Node* leaf, * junk;
-			std::cout << "\tKljuc na osnovu kojeg se vrsi pretraga: ";
+			std::cout << "\tKey to search: ";
 			std::cin >> key;
 			if (!b->SearchSingle(key, leaf, junk, &took_steps, &idx)) {
 
-				std::cout << std::endl << "\033[1;31mGreska, kljuc ne postoji\033[0m" << std::endl;
+				std::cout << std::endl << "\033[1;31mError, key doesn't exist\033[0m" << std::endl;
 				continue;
 			}
-			std::cout << "\tZa pretragu je bilo potrebno " << took_steps << " koraka. Podaci iz cvora: "<<std::endl;
+			std::cout << "\tSearch took " << took_steps << " steps. Record: "<<std::endl;
 			leaf->printNodeData(idx);
 			break;
 		}
@@ -112,28 +112,28 @@ void helpers::mainLoop() {
 			if (!nullCheckAndError(b)) continue;
 			size_t k, key, took_steps;
 			std::vector<size_t> keys;
-			std::cout << "\tBroj kljuceva na osnovu kojih se vrsi pretraga: ";
+			std::cout << "\tNumber of keys to search: ";
 			std::cin >> k;
 			keys.resize(k);
-			std::cout <<"\t"<<k << " vrednosti odvojenih blanko znakom: ";
+			std::cout <<"\t"<<k << " space separated values: ";
 			for (int i = 0; i < k; i++) {
 				std::cin >> key;
 				keys[i] = key;
 			}
 			b->SearchMultiple(keys, &took_steps);
-			std::cout << "\tZa pretragu je bilo potrebno " << took_steps << " koraka." << std::endl;
+			std::cout << "\tSearch took " << took_steps << " steps." << std::endl;
 			break;
 		}
 		case 7: {
 			if (!nullCheckAndError(b)) continue;
 			size_t k, startKey, took_steps;
-			std::cout << "\tPrimarni kljuc pocetnog zapisa: ";
+			std::cout << "\tStarting record primary key: ";
 			std::cin >> startKey;
-			std::cout << "\tBroj primarnih kljuceva k: ";
+			std::cout << "\tNumber of primary keys k: ";
 			std::cin >> k;
 			b->SearchMultipleSuccessive(startKey, k, &took_steps);
-			std::cout << "\tPodaci uspesno upisani u fajl" << std::endl;
-			std::cout << "\tZa pretragu je bilo potrebno "<<took_steps<<" koraka" << std::endl;
+			std::cout << "\tData successfully written to file" << std::endl;
+			std::cout << "\tSearch took "<<took_steps<<" steps" << std::endl;
 			break;
 		}
 		case 8: {
@@ -146,7 +146,7 @@ void helpers::mainLoop() {
 			delete b;
 			size_t m;
 			do {
-				std::cout << "\tRed stabla m: ";
+				std::cout << "\t B+ tree order: ";
 				std::cin >> m;
 			} while (m < 3 || m>10);
 			b = new BPlusTree(m);
